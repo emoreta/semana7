6b31334dcf9fad0fa5587d8530fe7cdd779cda9d
Edison Xavier Moreta Escobar <edison.moreta@quipu.de>
2022-05-24 12:22:37 -0500

    first commit

---
+++ S7EdisonMoreta9A.Android/SqLiteCliente.cs
@@ -0,0 +1,19 @@
+﻿using S7EdisonMoreta9A.Droid;
+using SQLite;
+using System.IO;
+
+[assembly: Xamarin.Forms.Dependency(typeof(SqLiteCliente))]
+namespace S7EdisonMoreta9A.Droid
+{
+    public class SqLiteCliente
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+
+            var path = Path.Combine(documentPath, "uisrael.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
---
+++ S7EdisonMoreta9A/Editar.xaml.cs
@@ -0,0 +1,69 @@
+﻿using S7EdisonMoreta9A.models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S7EdisonMoreta9A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Editar : ContentPage
+    {
+        private int idSelecccionado;
+        private SQLiteAsyncConnection _conn;
+        IEnumerable<Estudiante> ResultadoDelete;
+        IEnumerable<Estudiante> ResultadoUpdate;
+        public Editar(int id)
+        {
+            InitializeComponent();
+            idSelecccionado = id;
+        }
+        public static IEnumerable<Estudiante> Delete(SQLiteConnection db, int id)
+        {
+            return db.Query<Estudiante>("Delete from Estudiante where Id =  ?", id);
+        }
+
+        public static IEnumerable<Estudiante> Update(SQLiteConnection db, string nombre, string usuario, string contrasenia, int id)
+        {
+            return db.Query<Estudiante>("Update Estudiante SET Nombre = ?, Usuario = ?, Contrasenia = ? where Id =  ?", nombre, usuario, contrasenia, id);
+        }
+
+        private void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+
+            var db = new SQLiteConnection(databasePath);
+
+            ResultadoUpdate = Update(db, TxtNombre.Text, TxtUsuario.Text, TxtContrasenia.Text, idSelecccionado);
+
+            DisplayAlert("Alerta", "Se actualizó correctamente!", "OK");
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uisrael.db3");
+
+                var db = new SQLiteConnection(databasePath);
+
+                ResultadoDelete = Delete(db, idSelecccionado);
+
+                DisplayAlert("Alerta", "Se eliminó correctamente!", "OK");
+
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Alerta", "EROOR: " + ex.Message, "OK");
+
+            }
+        }
+    }
+}
